c3d34da6d8bbf7e940b9275665d1bdf8d594395a
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2023-12-20 20:08:38 -0700

    completed graphics for animals and hearts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import AnimalShow from "./AnimalShow";
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function getRandomAnimal(){
+    const animals = ['bird', 'cat', 'cow','dog','gator', 'horse'];
+    return animals[Math.floor(Math.random() * animals.length)];
 }
 
+function App(){
+    const [animals, setAnimals] = useState([]);
+    const handleClick = () => {
+        console.log("clicked");
+        setAnimals([...animals, getRandomAnimal()]);
+    }
+    const renderAnimals = animals.map((animal, index) => {
+        return <AnimalShow key={index} animalCount={animal} />
+    });
+    return (
+        <div className="app">
+          <button onClick={handleClick}>Add Animal</button>
+          
+          <div className="animal-list">{renderAnimals}</div>
+        </div>
+      );
+      
+}
 export default App;
