c292e886a4fd011d0ed03d7d6f33fa8c1e94fc8a
Adam Dockstader <39977252+AdamDock@users.noreply.github.com>
2023-12-20 20:09:30 -0700

    commit

---
+++ src/AnimalShow.js
@@ -0,0 +1,36 @@
+import {useState} from 'react';
+import bird from './svg/bird.svg';
+import cat from './svg/cat.svg';
+import cow from './svg/cow.svg';
+import dog from './svg/dog.svg';
+import gator from './svg/gator.svg';
+import horse from './svg/horse.svg';
+import heart from './svg/heart.svg';
+import './AnimalShow.css';
+
+const svgMap = {
+    bird: bird,
+    cat: cat,
+    cow: cow,
+    dog: dog,
+    gator: gator,
+    horse: horse,
+}
+function AnimalShow({animalCount}){  
+    const [clicks, setClicks] = useState(0);
+    const handleClick = () => {
+        setClicks(clicks + 1);
+    }
+    return (
+        <div className="animal-show"onClick={handleClick}>
+            <img className="animal" alt="animal" src={svgMap[animalCount]} />
+            <img 
+            className='heart'
+            alt="heart" 
+            src={heart}  
+            style={{width: 10 + 10*clicks+'px'}}/>
+            
+        </div>
+    );
+}
+export default AnimalShow
